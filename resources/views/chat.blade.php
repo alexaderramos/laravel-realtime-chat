77cdbbe35d7ff6fb3872d6d7cc4f71cbd62ce601
Alexander Ramos <tcristian_15.go@hotmail.com>
2020-03-06 23:52:16 -0500

    Chat template v1
    Sin scroll en el chat

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -7,20 +7,30 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Document</title>
+    <style>
+        .list-group{
+            overflow-y: scroll;
+            height: 200px;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
         <div class="row" id="app">
-            <ul class="list-group offset-4 col-4">
+            <div class=" offset-4 col-4">
                 <li class="list-group-item active">Chat Room</li>
-                <li class="list-group-item">Dapibus ac facilisis in</li>
-                <li class="list-group-item">Morbi leo risus</li>
-                <li class="list-group-item">Porta ac consectetur ac</li>
-                <li class="list-group-item">Vestibulum at eros</li>
-                <input type="text" placeholder="Ingresa tu mensage" class="form-control">
-            </ul>
+                <ul class="list-group">
+                    <message-component
+                        v-for="value in chat.message"
+                        :key=value.index
+                    >
+                        @{{ value }}
+                    </message-component>
+                </ul>
+                <input type="text" placeholder="Ingresa tu mensage" class="form-control" v-model="message" @keyup.enter="send">
+            </div>
         </div>
     </div>
-<script src="{{asset('js/app.js')}}"></script>
+<script src="{{asset('js/app.js')}}" ></script>
 </body>
 </html>
